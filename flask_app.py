107e3df07a67f22cc42376666cb71ae6e5c81dcf
Ryan Lee <ryanlee619@gmail.com>
2022-01-19 11:20:12 -0800

    return redirect nad send_file

--- flask_app.py
+++ flask_app.py
@@ -15,8 +15,8 @@ def index():
         try:
             get_cal(crn_content.split(" "))
             path = "classes.csv"
-            send_file(path, as_attachment=True)
-            return redirect(url_for('/'))
+            return send_file(path, as_attachment=True)
+            return redirect(url_for('/index.html'))
         except Exception as e:
             return "Either you misstyped your crns, or theres a problem on my end, contact nayr#8772 on discord or email ryanlee619@gmail.com for and problems or questions" + str(e)
         
