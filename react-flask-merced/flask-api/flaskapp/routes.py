b4cb0c91ddb405be55afd77f9ae5c4337144a462
Ryan Lee <ryanlee619@gmail.com>
2022-08-24 02:46:19 -0700

    deleted unnecesary comments in test function

--- react-flask-merced/flask-api/flaskapp/routes.py
+++ react-flask-merced/flask-api/flaskapp/routes.py
@@ -1,5 +1,5 @@
 from flaskapp import app, db
-from flask import Flask, render_template, url_for, request, redirect, send_file
+from flask import make_response, request
 from .tables import Subject
 from datetime import datetime, timedelta
 from .data import get_data, get_cookies
@@ -21,30 +21,26 @@ def test():
         Subject.query.delete()
         classes = get_data(get_cookies())
         subject_obj = parse_json(classes)
-        # exam_obj = Exam(datetime(2022, 8, 22, 15, 30), ['COB 210'])
-        # subject_obj = Subject(
-        #     crn=36566,
-        #     course_number = 'ANTH-005-01',
-        #     course_name = 'Biological Anthropology',
-        #     class_type = 'LECT',
-        #     days = [TUESDAY, THURSDAY],
-        #     start_time = datetime(2022, 8, 22, 15, 30),
-        #     time_delta = timedelta(hours=1, minutes=15),
-        #     location = 'CLSSRM 102',
-        #     end_date = datetime(2022, 12, 22),
-        #     instructor = 'Wooding, Stephen',
-        #     seats_avail = 0,
-        #     exam = exam_obj,
-        # )
         db.session.add_all(subject_obj)
         db.session.commit()
 
-@app.route("/getcsv", methods=['GET'])
+
+
+@app.route("/getcsv", methods=['POST'])
 def getcsv():
-    if request.method == 'GET':
-        crns = ['31406', '31407', '30033', '30036', '33957', '30139', '30141', '30248', '30588']
+    if request.method == 'POST':
+        print(request.data)
+        crns = set(request.json['crns'])
+        print(crns)
+        # crns = ['31406', '31407', '30033', '30036', '33957', '30139', '30141', '30248', '30588']
         df = create_df(crns)
-        print(df)
-        return df.to_html()
+        df_csv = create_df(crns).to_csv(index=False)
+        # return send_file(df_csv, mimetype='text/csv',as_attachment=True)
+        response = make_response(df_csv)
+        response.headers['Content-Disposition'] = 'attachment; filename=classes.csv'
+        response.mimetype = 'text/csv'
+        # return the Response object
+        return response
+        # return df.to_html()
         # df = create_df()
         # print(df['Subject'])
