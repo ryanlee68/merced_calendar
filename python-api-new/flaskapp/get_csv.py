da43b669e705d8448fe15b3a5c751f11b9649c92
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-08-23 23:19:00 -0700

    Updated `map_dates` function to take in a pandas.Series instead of single crn (as an integer)
    This function was changed because querying the database (as was done in the previously implementation) was wasteful and slow.  Here we are accessing this data from the DataFrame rather than querying the same date we have already queried previously.
    
    Co-authored-by: Ryan Lee <54428111+ryanlee68@users.noreply.github.com>

--- python-api-new/flaskapp/get_csv.py
+++ python-api-new/flaskapp/get_csv.py
@@ -1,41 +1,18 @@
 # file that takes in crn numbers and returns a csv
+from dateutil.rrule import rrule, WEEKLY
+from sqlalchemy import select
+from .tables import Subject
 from flaskapp import db
 import pandas as pd
-from .tables import Subject
-from sqlalchemy import select
-from dateutil.rrule import rrule, WEEKLY
-from datetime import datetime
 
-# def get_csv(crns):
-    # print(Subject.query.all())
-    # print(type(Subject.query.get(31406).course_name))
-    # columns = ["Subject", "Start date", "Start Time", "End Time", "Description", "Location"]
-    # main_df = pd.DataFrame(columns=columns)
-    # exam_description = 'Good luck with your exams!'
-    # # for crn in crns:
-    # subject = Subject.query.get(crn)
-    # name = f"{subject.course_name[:20]}-{subject.class_type}"
-    # start_time = subject.start_time
-    # end_time = subject.end_time
-    # days = subject.days
-    # description = f"CRN: {crn} Course #: {subject.course_number} Instructor(s): {subject.instructor}"
-    # exam_time = subject.exam_time
-    # exam_end_time = subject.exam_end_time
-    # exam_location = subject.exam_location
-
-def map_dates(crn):
-    subject = Subject.query.get(crn)
-    # subject.start_time
-    # TODO use rruleset to exclude holidays
-    # print(f"{subject.days}")
+def map_dates(series: pd.Series):
     dates = list(rrule(
         WEEKLY,
-        byweekday=(subject.days),
-        dtstart=subject.start_time,
-        until=subject.end_time
+        byweekday=(series['days']),
+        dtstart=series['start_time'].date(),
+        until=series['end_time'].date()
     ))
     return dates
-    # print(dates)
 
 def create_df(crns):
     df = pd.read_sql(
